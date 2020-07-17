f8c39fe8de71954f33445673c2389126df42f4d9
Lai Wei Lee <laiwei>
2020-07-17 15:18:08 +0800

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 //props.children refers to the value typed bwt the <button><>
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+    <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
         {
             isGoogleSignIn ? <i className="fab fa-google"></i> : children
         }
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,17 +1,25 @@
 import { CartActionTypes } from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 //weird syntax in parameter is for DEFAULT VALUE. ES6 syntax
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case CartActionTypes.TOGGLE_CART_HIDDEN :
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden        
-            }
+            };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                //this has to return a new array! TO TRIGGER RE-RENDER FOR REACT
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
         default:
             return state;
     }
